9fa786217200d281b83c8e5508772d0d0045b38f
Elliot Chi <elliotschi@gmail.com>
2016-03-03 21:24:29 -0800

    feat: angular 2 todo

---
+++ client/app/todo-input.ts
@@ -0,0 +1,25 @@
+import { Component } from 'angular2/core';
+import {TodoService} from './todo-service';
+
+@Component({
+  selector: 'todo-input',
+  template: `<div>
+    <form (submit)=onSubmit()>
+      <input type="text" [(ngModel)]="todoModel">
+     </form>
+    </div>`
+})
+
+export class TodoInput{
+  todoModel; 
+
+  constructor(public todoService: TodoService){
+    console.log(todoService);
+
+  }
+
+  onSubmit(value) {
+    this.todoService.todos.push(this.todoModel);
+    console.log(this.todoService.todos);
+  }
+}
---
+++ client/app/todo-model.ts
@@ -0,0 +1,6 @@
+export class TodoModel{
+  status:string = 'started';
+  // title:string = '';
+
+  constructor(public title:string =''){}
+}
