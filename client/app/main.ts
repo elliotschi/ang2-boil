2a7cf506a7ce34df79e94e5b85b05a28bab12312
Elliot Chi <elliotschi@gmail.com>
2016-03-08 17:52:01 -0800

    chore: fix folder structure for features and components

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,10 +1,10 @@
 import {bootstrap} from "angular2/platform/browser";
 import {Component} from "angular2/core";
-import {TodoInput} from './todo-input';
-import {TodoService} from './todo-service';
-import {TodoList} from './todo-list';
-import {StatusSelector} from './status-selector';
-import {SearchBox} from './search-box';
+import {TodoInput} from './todo/components/todo-input';
+import {TodoService} from './todo/services/todo-service';
+import {TodoList} from './todo/components/todo-list';
+import {StatusSelector} from './todo/components/status-selector';
+import {SearchBox} from './search/components/search-box';
 
 @Component({
     selector: 'app',
--- client/app/todo/components/todo-list.ts
+++ client/app/todo/components/todo-list.ts
@@ -1,8 +1,8 @@
 import {Component, Input} from 'angular2/core';
-import {TodoService} from './todo-service';
+import {TodoService} from '../services/todo-service';
 import {TodoItemRenderer} from './todo-item-renderer';
-import {SearchPipe} from './search-pipe';
-import {StartPipe} from './start-pipe';
+import {SearchPipe} from '../../search/pipes/search-pipe';
+import {StartPipe} from '../pipes/start-pipe';
 
 @Component({
   selector: 'todo-list',
